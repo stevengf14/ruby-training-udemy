59733f517063a88f61c7f331dfa6a0c3312caeba
Steven <stevengf14@gmail.com>
2023-01-30 22:08:00 -0500

    Hello Ruby

---
+++ hello.rb
@@ -0,0 +1,15 @@
+# puts "Hello World"
+
+# puts "What is your name?"
+print "What is your name?"
+
+name = gets
+puts name
+
+# Interpolation
+name = "Steven"
+puts "hello #{name}"
+
+puts "What is your name?"
+name = gets
+puts "hi #{name}"
