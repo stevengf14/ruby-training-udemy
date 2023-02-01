c4140685bc543045cd6814b57a1dc184d5c57603
Steven <stevengf14@gmail.com>
2023-01-31 19:19:28 -0500

    input_output

---
+++ input_output.rb
@@ -0,0 +1,18 @@
+name = "Steven"
+puts "Hello #{name}, you are a 'fullstack developer'"
+puts "Hello #{name}, you are a \"fullstack developer\""
+
+# string => integer
+"100".to_i # => 100
+
+# string => float
+"100".to_f # => 100.0
+
+#integer => stirng 
+"#{100}" # => "100"
+
+=begin
+This is a comment 
+This is also a comment    
+All of this is a comment block
+=end
