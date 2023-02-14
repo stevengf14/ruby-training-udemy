08316c6136d5d70608a0c6687c91ff82e36513f6
Steven <stevengf14@gmail.com>
2023-02-13 22:53:20 -0500

    inspecting objects

---
+++ inspecting_objects.rb
@@ -0,0 +1,22 @@
+class User
+    attr_accessor :name, :age, :email
+
+    def initialize(name, age, email)
+        self.name = name
+        self.age = age
+        self.email = email
+    end
+
+    def to_s
+        "#{self.name} - #{self.email} - #{self.age}"
+    end
+
+    def inspect
+        "<#{self.email}>"
+    end
+end
+
+user = User.new("Steven", 20, "steven@email.com")
+user2 = User.new("Andres", 25, "andres@email.com")
+puts user
+p user2
